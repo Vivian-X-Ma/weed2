faa92a8085ae1ab14066440375c45c927b64d616
Vivian Ma <vivianxma@gmail.com>
2025-03-01 18:36:38 -0600

    trying to deploy on gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,8 @@ const App = () => {
   useEffect(() => {
     tf.ready().then(async () => {
       const yolov8 = await tf.loadGraphModel(
-        `${window.location.href}/${modelName}_web_model/model.json`,
+        //IMPORTANT NOTE: This  --> ${window.location.href} --> was originally where the "./" is. I changed it to "./" because for the hugging face deployment, the path was wrong. It needs to directly go into best_web_model folder, use relative path. 
+        `./${modelName}_web_model/model.json`,
         {
           onProgress: (fractions) => {
             setLoading({ loading: true, progress: fractions }); // set loading fractions
